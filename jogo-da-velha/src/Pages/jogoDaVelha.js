bab139f15ac0e93fd4d9a9e089134727be858717
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2023-03-10 15:39:48 -0300

    FEAT: Lógica do jogo

--- jogo-da-velha/src/Pages/jogoDaVelha.js
+++ jogo-da-velha/src/Pages/jogoDaVelha.js
@@ -1,9 +1,72 @@
-import { BodyStyled } from "./styled"
+import { useState, useEffect } from "react"
+import { BodyStyled, DivBase } from "./styled"
 
 export const JogoDaVelha=()=>{
+
+    const arr = Array(9).fill(" ");
+
+    const [base, setBase] = useState(arr);
+    const [player, setPlayer]=useState("O")
+    const [winner, setWinner]=useState()
+
+    const handleClick =(index)=>{
+        if(winner){
+            console.log("Jogo finalizado");
+            return null;
+        }
+
+        if(base[index] !== " "){
+            return null
+        }
+        setBase(base.map((item, itemIndex)=> itemIndex === index ? player : item));
+
+        setPlayer(player === "X" ? "O" : "X")
+    }
+
+        const checkWinner=() =>{
+
+            const possibleWaysToWin = [
+                [base[0], base[1], base[2]],
+                [base[3], base[4], base[5]],
+                [base[6], base[7], base[8]],
+
+                [base[0], base[3], base[6]],
+                [base[1], base[4], base[7]],
+                [base[2], base[5], base[8]],
+
+                [base[0], base[4], base[8]],
+                [base[2], base[4], base[6]],
+
+            ];
+            
+            possibleWaysToWin.forEach(cells =>{
+                if(cells.every(cell => cell === "O")){
+                    setWinner("O venceu")
+                }else if(cells.every(cell => cell === "X")){
+                    setWinner("X venceu")
+                }else{
+                    console.log("Deu velha!")
+                }
+            })
+    }
+
+    useEffect(checkWinner, [base])
+
+
     return(
         <BodyStyled>
             <h1>Jogo da velha</h1>
+
+            <DivBase {...winner? "game-over" : ""}>
+                {base.map((item, index)=>(
+                    <div 
+                        key={index} 
+                        className={`cell ${item}`}
+                        onClick={() => handleClick(index)}>
+                        {item}
+                    </div>
+            ))}
+            </DivBase>
         </BodyStyled>
     )
 }
--- jogo-da-velha/src/Pages/styled.js
+++ jogo-da-velha/src/Pages/styled.js
@@ -12,6 +12,36 @@ export const BodyStyled = styled.div`
     h1{
         text-align: center;
         color: #7BB0E6;
+   }
+`
+export const DivBase = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 240px);
+    grid-template-rows: repeat(3, 200px);
+    gap: 1rem;
 
+    .cell{
+        display: grid;
+        align-items: center;
+        justify-content: center;
+        background-color: white;
+        border-radius: 8px;
+        font-size: 20px;
+        font-weight: bolder;
+        color: black;
+        cursor: pointer;
+    }
+
+    .cell:not(.O):not(.X):hover{
+        box-shadow: 0 20px 25px -5px rgba(0,0,0,0.1);
+    }
+    .cell.O, .cell.X{
+        cursor: not-allowed;
+    }
+    .cell.O{
+        background-color: var(--0-color);
+    }
+    .cell.X{
+        background-color: var(--X-color);
     }
 `
